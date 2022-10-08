0f5495e18266005bd26f766626859c5430978e39
Kinga89 <kinga-kot@hotmail.com>
2022-10-08 22:12:39 +0200

    scroll to function

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+/*SCROLLING TO SPECIFIC PAGE SECTION*/
+const about = document.querySelector("#about");
+about.addEventListener("click", () => {
+    const aboutMeHeading = document.querySelector(".aboutme-heading");
+    aboutMeHeading.scrollIntoView({behavior:"smooth"});
+});
+
+const projects = document.querySelector("#projects");
+projects.addEventListener("click", () => {
+    const projectsHeading = document.querySelector(".projects-heading");
+    projectsHeading.scrollIntoView({behavior:"smooth"});
+});
+
+const skills = document.querySelector("#skills");
+skills.addEventListener("click", () => {
+    const skillsHeading = document.querySelector(".skills-heading");
+    skillsHeading.scrollIntoView({behavior:"smooth"});
+});
+
+
+/*VIDEO IN MOTION WHILE HOVERED ON*/
 const video = document.querySelectorAll("video");
 
 for (let i = 0; i < video.length; i++){
